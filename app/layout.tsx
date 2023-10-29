874b050f6fc6db0a8e733503ddaff24effed7efa
tolgattli <tolgatatli44@gmail.com>
2023-10-30 01:50:12 +0300

    next-auth-navbar

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import "@/styles/globals.css";
+
+import Nav from "@/components/Nav";
+import Provider from "@/components/Provider";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+  
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
